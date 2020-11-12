56f5c5c7b4f8f953b3ff651bbb7840e66ea32219
viniciusmaurente <viniciusvmaurente@gmail.com>
2020-11-11 21:32:45 -0300

    Configuração inicial App Desafio4all, implementação de dependências FireBase grandle

---
+++ app/src/main/java/com/br/desafio4all/MainActivity.java
@@ -0,0 +1,15 @@
+package com.br.desafio4all;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+//create by vinicius vasconcelos maurente - viniciusvmaurente@gmail.com
