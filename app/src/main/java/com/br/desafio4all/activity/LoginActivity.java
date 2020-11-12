893632a61ffdbfc555b45a85376dfbc97c40a0ed
viniciusmaurente <viniciusvmaurente@gmail.com>
2020-11-12 12:44:02 -0300

    criando interface de login

---
+++ app/src/main/java/com/br/desafio4all/activity/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.br.desafio4all.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.br.desafio4all.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
+
+
+//create by vinicius vasconcelos maurente - viniciusvmaurente@gmail.com
--- app/src/main/java/com/br/desafio4all/activity/MainActivity.java
+++ app/src/main/java/com/br/desafio4all/activity/MainActivity.java
@@ -1,16 +1,30 @@
-package com.br.desafio4all;
+package com.br.desafio4all.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.br.desafio4all.R;
+
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
     }
 }
-// 
+
+
+
 //create by vinicius vasconcelos maurente - viniciusvmaurente@gmail.com
