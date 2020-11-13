ff83c0513d84a207afd9859c6d405b6c51672842
viniciusmaurente <viniciusvmaurente@gmail.com>
2020-11-12 22:33:09 -0300

    finalizando interface cadastro e configurando persistência de dados dos métodos de login e cadastro de usuário no Firebase

--- app/src/main/java/com/br/desafio4all/activity/CadastroActivity.java
+++ app/src/main/java/com/br/desafio4all/activity/CadastroActivity.java
@@ -1,16 +1,141 @@
 package com.br.desafio4all.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.br.desafio4all.R;
+import com.br.desafio4all.model.Usuario;
+import com.br.desafio4all.util.ConfiguracaoFirebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    private EditText campoNome, campoEmail, campoSenha;
+    private Button botaoCadastrar;
+    private ProgressBar progressBar;
+
+    private Usuario usuario;
+
+    private FirebaseAuth autenticacao;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        inicializarComponentes();
+
+        //cadastro usuario
+        progressBar.setVisibility(View.GONE);
+        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String textoNome = campoNome.getText().toString();
+                String textoEmail = campoEmail.getText().toString();
+                String textoSenha = campoSenha.getText().toString();
+
+                if (!textoNome.isEmpty()) {
+                    if (!textoEmail.isEmpty()) {
+                        if (!textoSenha.isEmpty()) {
+                            usuario = new Usuario();
+                            usuario.setNome(textoNome);
+                            usuario.setEmail(textoEmail);
+                            usuario.setSenha(textoSenha);
+                            cadastrar(usuario);
+
+                        } else {
+                            Toast.makeText(CadastroActivity.this,
+                                    "Preencha a senha!",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    } else {
+                        Toast.makeText(CadastroActivity.this,
+                                "Preencha o e-mail!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(CadastroActivity.this,
+                            "Preencha o nome!",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    /**
+     * Método responsável por cadastrar usuário com e-mail e senha
+     * e fazer validações ao fazer o cadastro
+     */
+    public void cadastrar(Usuario usuario) {
+        progressBar.setVisibility(View.VISIBLE);
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.createUserWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(
+                this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            progressBar.setVisibility(View.GONE);
+                            Toast.makeText(CadastroActivity.this,
+                                    "Cadastro com sucesso!",
+                                    Toast.LENGTH_SHORT).show();
+
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            finish();
+
+                        } else {
+                            progressBar.setVisibility(View.GONE);
+
+                            String erroExcecao = "";
+                            try {
+                                throw task.getException();
+                            } catch (FirebaseAuthWeakPasswordException e) {
+                                erroExcecao = "Digite uma senha mais forte!";
+                            } catch (FirebaseAuthInvalidCredentialsException e) {
+                                erroExcecao = "Por favor, digite um e-mail válido.";
+                            } catch (FirebaseAuthUserCollisionException e) {
+                                erroExcecao = "Esta conta já foi cadastrada.";
+                            } catch (Exception e) {
+                                erroExcecao = "ao cadastrar usuário: " + e.getMessage();
+                                e.printStackTrace();
+                            }
+                            Toast.makeText(CadastroActivity.this,
+                                    "Erro: " + erroExcecao,
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+        );
+
+    }
+
+
+    public void inicializarComponentes() {
+        campoNome = findViewById(R.id.editCadastroNome);
+        campoEmail = findViewById(R.id.editCadastroEmail);
+        campoSenha = findViewById(R.id.editCadastroSenha);
+        botaoCadastrar = findViewById(R.id.buttonCadastrar);
+        progressBar = findViewById(R.id.progressCadastro);
+
+        campoNome.requestFocus();
     }
 }
